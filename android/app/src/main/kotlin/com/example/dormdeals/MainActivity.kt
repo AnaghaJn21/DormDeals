fe94d593ee9df595051b606291b2dd17140885f2
jithinpkumar <jithinpkbtw25@gmail.com>
2024-06-28 10:32:01 +0530

    my part

---
+++ android/app/src/main/kotlin/com/example/dormdeals/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dormdeals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:dormdeals/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "DormDeals",
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text("login")),
+            ElevatedButton(onPressed: () {}, child: Text("signup"))
+          ],
+        ),
+      ),
+    );
+  }
+}
