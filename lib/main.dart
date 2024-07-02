5b7597aea03c9ce098ed7f8ad01f6afe47f7f2a2
AnaghaJn21 <anaghajayan21@gmail.com>
2024-07-02 17:39:35 +0530

    ABout Us Page Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:dormdeals/pages/About_Pg.dart';
 import 'package:dormdeals/pages/Buy_Main_Pg.dart';
 import 'package:dormdeals/pages/Profile_Pg.dart';
 import 'package:dormdeals/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:dormdeals/pages/About_Pg.dart';
 
 void main() {
   runApp(const MyApp());
